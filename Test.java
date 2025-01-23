8ddd645623feeed32d736b82adbcd34c291ecd81
risav-pyakurel <pyakurel.risav@gmail.com>
2025-01-23 23:23:24 +0545

    java new

--- Test.java
+++ Test.java
@@ -1,7 +1,17 @@
 public class Test {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    int x =10;
 
+    public static void main(String[] args) {
+        System.out.println("Hello World"); 
+
+    }
+
+    public static void main1(String[] args) {
+        System.out.println("Hello World 1");
+    }
+
+    public static void main2(String[] args) {
+        System.out.println("Hello World 2");
     }
 }
