34c704dd9f8ea8dd4630f0cf11155c513107b0d1
risav-pyakurel <pyakurel.risav@gmail.com>
2025-01-29 16:22:52 +0545

    new java

--- Test.java
+++ Test.java
@@ -1,27 +1,44 @@
+// public class Test {
+
+//     int x =10;
+
+//     public static void main(String[] args) {
+
+//     int a = 1;
+//     System.out.println(Integer.toString(a));
+//     String name = "Risav Pyakurel";
+//     System.out.println(name);
+
+//     for (int i =1; i<=10; i++){
+//         System.out.println(i);
+
+//     }
+//     int i=1;
+
+//     while (i++<10) {
+//         System.out.println("L");
+//     }
+//     }
+// }
+
+import java.util.*;
+
+
 public class Test {
-
-    int x =10;
-
     public static void main(String[] args) {
+        List <Object> arr = new ArrayList<>();
+        char [] arr1 = {'a','b','c'};
 
-    int a = 1;
-    System.out.println(Integer.toString(a));
-    String name = "Risav Pyakurel";
-    System.out.println(name);
+        for(int i =0; i<=arr1.length; i++){
+            System.out.println(arr1[i]);
 
-    for (int i =1; i<=10; i++){
-        System.out.println(i);
 
-    }
-    int i=1;
-
-    while (i++<10) {
-        System.out.println("L");
-    }
-
+        }
 
 
 
 
     }
+
+
 }
