b2b29c202b5370508def6b982c136cf5e10bb649
risav-pyakurel <pyakurel.risav@gmail.com>
2025-01-29 16:35:58 +0545

    map

--- Test.java
+++ Test.java
@@ -27,13 +27,17 @@ import java.util.*;
 public class Test {
     public static void main(String[] args) {
         List <Object> arr = new ArrayList<>();
-        char [] arr1 = {'a','b','c'};
+        Map <String, Integer> marks = new HashMap<>();
 
-        for(int i =0; i<=arr1.length; i++){
-            System.out.println(arr1[i]);
+        marks.put("risav", 100);
+        marks.put("sam" , 50);
+        // char [] arr1 = {'a','b','c'};
+
+        // for(int i =0; i<=arr1.length; i++){
+        //     System.out.println(arr1[i]);
 
 
-        }
+        // }
 
 
 
