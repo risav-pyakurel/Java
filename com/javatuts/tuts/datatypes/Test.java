9a6bbc3c965b1a07b8dc1f721915123e164b92d4
risav-pyakurel <pyakurel.risav@gmail.com>
2025-02-05 23:16:03 +0545

    learning integral numbers

--- com/javatuts/tuts/datatypes/Test.java
+++ com/javatuts/tuts/datatypes/Test.java
@@ -1,7 +1,12 @@
 package com.javatuts.tuts.datatypes;
 
-public class Test {
+public class  Test {
     public static void main (String[] args){
-        System.out.println("Hello World");
+        int a = 1;
+        byte b = 1;
+        short c = 1;
+        long d = 1;
+
+        System.out.println(c+d);
     }
 }
