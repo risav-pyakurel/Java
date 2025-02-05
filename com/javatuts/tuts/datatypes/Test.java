7367d84ec7bf51e5727151d21074b8c45338b314
risav-pyakurel <pyakurel.risav@gmail.com>
2025-02-05 23:31:32 +0545

    boolean

--- com/javatuts/tuts/datatypes/Test.java
+++ com/javatuts/tuts/datatypes/Test.java
@@ -2,11 +2,8 @@ package com.javatuts.tuts.datatypes;
 
 public class  Test {
     public static void main (String[] args){
-        int a = 1;
-        byte b = 1;
-        short c = 1;
-        long d = 1;
+        boolean isAdult = false;
+        isAdult = true;
 
-        System.out.println(c+d);
     }
 }
