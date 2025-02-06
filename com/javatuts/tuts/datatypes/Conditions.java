878f77cc0152491e7b7536cdc0e1e26793ed6eb8
risav-pyakurel <pyakurel.risav@gmail.com>
2025-02-06 23:25:17 +0545

    conditionals

---
+++ com/javatuts/tuts/datatypes/Conditions.java
@@ -0,0 +1,18 @@
+package com.javatuts.tuts.datatypes;
+
+public class Conditions {
+    public static void main(String[] args) {
+        boolean isSunny = true;
+        boolean isWarm = false;
+
+        if(isSunny &&isWarm) {
+            System.out.println("It's a beach day");
+
+        }
+        else {
+            System.out.println("It's not a good day to go beach");
+        }
+
+    }
+
+}
