8c3464765a19ea7e59c770b466ebd6ba5356393b
risav-pyakurel <pyakurel.risav@gmail.com>
2025-02-06 21:50:53 +0545

    pre and post

---
+++ com/javatuts/tuts/datatypes/Operator.java
@@ -0,0 +1,21 @@
+package com.javatuts.tuts.datatypes;
+
+public class Operator {
+    public static void main(String[] args) {
+        int a  = 5+3;
+        int b=  10-4;
+        int c= 6*7;
+        int d = 10/3;
+        double e = 10.0/3.0;
+        int f = 10 % 3;
+        System.out.println(a);
+        a++;
+        System.out.println(a);
+        ++a;
+        System.out.println(a);
+        a--;
+        System.out.println(a);
+        --a;
+        System.out.println(a);
+    }
+}
