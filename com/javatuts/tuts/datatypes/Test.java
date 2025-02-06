831d14a39c04c6ae6394a14df90c558de675f37e
risav-pyakurel <pyakurel.risav@gmail.com>
2025-02-06 21:41:17 +0545

    string immutiablity

--- com/javatuts/tuts/datatypes/Test.java
+++ com/javatuts/tuts/datatypes/Test.java
@@ -3,12 +3,19 @@ package com.javatuts.tuts.datatypes;
 
 public class  Test {
     public static void main (String[] args){
-    String str1 = "hello";
-    String str2 = "hello";
-    String str3 = new String("hello");
 
-        System.out.println(str1 == str2);
-        System.out.println(str1 == str3);
+//    String str1 = "hello";
+//    String str2 = "hello";
+//    String str3 = new String("hello");
+//
+//        System.out.println(str1 == str2);
+//        System.out.println(str1 == str3);
+//
+//
+        String a = "Hello World";
+        String b = a.toUpperCase();
+
+
 
 
     }
