41832e62b23fd97fa5277eeabfd9a75c146dc260
risav-pyakurel <pyakurel.risav@gmail.com>
2025-02-08 23:20:00 +0545

    loops

--- com/javatuts/tuts/datatypes/Loop.java
+++ com/javatuts/tuts/datatypes/Loop.java
@@ -2,7 +2,13 @@ package com.javatuts.tuts.datatypes;
 
 public class Loop {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int counter = 0;
+        while (counter>10) {
+            System.out.println("hellp world");
+            counter += 1;
+        }
+
+
 
 
     }
